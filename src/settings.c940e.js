9e7ec8ff755a3c8131af78052b66878b26c7b30d
unknown <541614009@qq.com>
2020-06-05 21:42:15 +0800

    changeposition

---
+++ src/settings.c940e.js
@@ -0,0 +1,156 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "14TDKXr2NJ6LjvHPops74o": [
+                "effects/builtin-2d-gray-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            "0ek66qC1NOQLjgYmi04HvX": [
+                "effects/builtin-2d-spine.effect",
+                "cc.EffectAsset"
+            ],
+            "28dPjdQWxEQIG3VVl1Qm6T": [
+                "effects/builtin-2d-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            "2afAA24LNP4YmYiaVLiivs": [
+                "effects/builtin-3d-trail.effect",
+                "cc.EffectAsset"
+            ],
+            c0BAyVxX9JzZy8EjFrc9DU: [
+                "effects/builtin-clear-stencil.effect",
+                "cc.EffectAsset"
+            ],
+            "6dkeWRTOBGXICfYQ7JUBnG": [
+                "effects/builtin-unlit.effect",
+                "cc.EffectAsset"
+            ],
+            "6fgBCSDDdPMInvyNlggls2": [
+                "materials/builtin-2d-base.mtl",
+                "cc.Material"
+            ],
+            "3ae7efMv1CLq2ilvUY/tQi": [
+                "materials/builtin-2d-gray-sprite.mtl",
+                "cc.Material"
+            ],
+            "7a/QZLET9IDreTiBfRn2PD": [
+                "materials/builtin-2d-spine.mtl",
+                "cc.Material"
+            ],
+            "ecpdLyjvZBwrvm+cedCcQy": [
+                "materials/builtin-2d-sprite.mtl",
+                "cc.Material"
+            ],
+            "46bU+b5fROqIXVPG6aZWWK": [
+                "materials/builtin-3d-trail.mtl",
+                "cc.Material"
+            ],
+            cffgu4qBxEqa150o1DmRAy: [
+                "materials/builtin-clear-stencil.mtl",
+                "cc.Material"
+            ],
+            "2aKWBXJHxKHLvrBUi2yYZQ": [
+                "materials/builtin-unlit.mtl",
+                "cc.Material"
+            ]
+        }
+    },
+    launchScene: "db://assets/myscene.fire",
+    scenes: [
+        {
+            url: "db://assets/myscene.fire",
+            uuid: "74jkWw/olCI5cMMgIfvTN5"
+        }
+    ],
+    packedAssets: {
+        "079499991": [
+            "2aKWBXJHxKHLvrBUi2yYZQ",
+            "6dkeWRTOBGXICfYQ7JUBnG"
+        ],
+        "07ce7530a": [
+            "14TDKXr2NJ6LjvHPops74o",
+            "3ae7efMv1CLq2ilvUY/tQi"
+        ],
+        "07feb2445": [
+            "11uCRMttlFd7N+YnuVInhI",
+            "42ZifUFO9HG6/unBZYJRqs",
+            "49LBk7l6RC87epam5M7MDp",
+            "74jkWw/olCI5cMMgIfvTN5",
+            "7eCqm3QVdFt72eOEh1lJcS",
+            "8fi6VPcPZMoYbZ3dvYXnad",
+            "97fP6bIcdJjrBgVBgAURqL",
+            "a70y/BSilJHqXiCJIGPTXQ"
+        ],
+        "0a5cba09d": [
+            "2afAA24LNP4YmYiaVLiivs",
+            "46bU+b5fROqIXVPG6aZWWK"
+        ],
+        "0d669730c": [
+            "c0BAyVxX9JzZy8EjFrc9DU",
+            "cffgu4qBxEqa150o1DmRAy"
+        ],
+        "0deef8002": [
+            "02delMVqdBD70a/HSD99FK",
+            "0a8z+LRRJFL4/64Bdc7nZ8",
+            "3e2Ug6031GmYdik5kQw+En",
+            "80x7HnHfdPx5TlxTBzhCDt",
+            "c6tf0Tan9Bh4cWEYnl0Twn",
+            "f3d5R3fb9HUZAvmFlzcgFS"
+        ],
+        "0e4bc3b03": [
+            "0ek66qC1NOQLjgYmi04HvX",
+            "7a/QZLET9IDreTiBfRn2PD"
+        ]
+    },
+    md5AssetsMap: {
+        import: [
+            "079499991",
+            "6ed06",
+            "07ce7530a",
+            "97339",
+            "07feb2445",
+            "1bc11",
+            "0a5cba09d",
+            "4c931",
+            "0d669730c",
+            "661c4",
+            "0deef8002",
+            "5075e",
+            "0e4bc3b03",
+            "92de1",
+            "28dPjdQWxEQIG3VVl1Qm6T",
+            "d3cc0",
+            "6fgBCSDDdPMInvyNlggls2",
+            "446f8",
+            "ecpdLyjvZBwrvm+cedCcQy",
+            "f3308"
+        ],
+        "raw-assets": [
+            "02delMVqdBD70a/HSD99FK",
+            "cea68",
+            "0a8z+LRRJFL4/64Bdc7nZ8",
+            "0c687",
+            "3e2Ug6031GmYdik5kQw+En",
+            "5fdc8",
+            "80x7HnHfdPx5TlxTBzhCDt",
+            "c0cb3",
+            "c6tf0Tan9Bh4cWEYnl0Twn",
+            "4b0bd",
+            "f3d5R3fb9HUZAvmFlzcgFS",
+            "c1880"
+        ]
+    },
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
